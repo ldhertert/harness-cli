b8f1662580dde11013f652ad61659dd9d7475b52
Luke Hertert <luke.hertert@gmail.com>
2020-09-02 17:12:29 -0400

    Add additional flags

--- src/commands/git/clone.ts
+++ src/commands/git/clone.ts
@@ -6,6 +6,10 @@ export default class GitClone extends Command {
 
   static flags = {
     ref: flags.string({ default: 'master' }),
+    cwd: flags.string({ }),
+    token: flags.string({ description: 'Github personal access token'}),
+    username: flags.string({ description: 'Github username'}),
+    password: flags.string({ description: 'Github password'}),
   }
 
   static args = [{name: 'repo'}]
@@ -15,8 +19,10 @@ export default class GitClone extends Command {
 
     const git = new Git(args.repo, {
       ref: flags.ref,
-      cwd: '/Users/lukehertert/code/harness-cli/workspace',
-      auth: {},
+      cwd: flags.cwd || '/Users/lukehertert/code/harness-cli/workspace',
+      auth: {
+        token: flags.token,
+      },
     })
     await git.clone()
   }
--- src/util/git.ts
+++ src/util/git.ts
@@ -46,31 +46,31 @@ export class Git {
     })
   }
 
-//   module.exports.addAll = async function addAll(dir, glob) {
-//     let files = await util.glob(glob || "**/*.yaml", dir);
-//     for (const filepath of files) {
-//         await git.add({ fs, dir, filepath })
-//     }
-// }
+  //   module.exports.addAll = async function addAll(dir, glob) {
+  //     let files = await util.glob(glob || "**/*.yaml", dir);
+  //     for (const filepath of files) {
+  //         await git.add({ fs, dir, filepath })
+  //     }
+  // }
 
-// module.exports.commit = async function commit(message, directory, author) {
-//     await git.commit({ 
-//         fs, 
-//         dir: directory, 
-//         message: message,
-//         author: author || {
-//             name: 'Harness Automation',
-//             email: 'no-reply@harness.io',
-//           },
-       
-//     });
-// }
+  // module.exports.commit = async function commit(message, directory, author) {
+  //     await git.commit({
+  //         fs,
+  //         dir: directory,
+  //         message: message,
+  //         author: author || {
+  //             name: 'Harness Automation',
+  //             email: 'no-reply@harness.io',
+  //           },
+
+  //     });
+  // }
 
 // module.exports.push = async function push(repo, token, directory) {
-//     await git.push({ 
-//         fs, 
-//         http, 
-//         dir: directory, 
+//     await git.push({
+//         fs,
+//         http,
+//         dir: directory,
 //         url: repo,
 //         onAuth: url => {
 //             return { username: token, password: "" }
