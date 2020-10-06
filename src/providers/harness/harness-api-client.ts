eb804945c542f616213bfcb7c2031a0fdfcf2d30
Luke Hertert <luke.hertert@gmail.com>
2020-10-05 23:01:58 -0400

    Add username/password auth

--- src/providers/harness/harness-api-client.ts
+++ src/providers/harness/harness-api-client.ts
@@ -7,18 +7,24 @@ import { Environments } from './environments'
 import { CloudProviders } from './cloud-providers'
 import { Groups } from './groups'
 import { Users } from './users'
+import axios from 'axios'
 
 export interface HarnessApiOptions {
     accountId: string,
-    apiKey: string,
+    apiKey?: string,
+    username?: string,
+    password?: string,
     managerUrl?: string,
 }
 
+const defaultManagerUrl = 'https://app.harness.io'
+
 export class Harness {
-    protected managerUrl: string;
-    protected apiKey: string;
-    protected accountId: string;
-    protected gql: GraphQLClient;
+    managerUrl: string;
+    accountId: string;
+    protected apiKey?: string;
+    protected bearerToken?: string;
+    client: GraphQLClient;
 
     applications: Applications;
     connectors: { git: GitConnectors; };
@@ -29,22 +35,47 @@ export class Harness {
     users: Users
 
     constructor(options: HarnessApiOptions) {
-        this.managerUrl = new URL(options.managerUrl || 'https://app.harness.io').origin
-        this.accountId = options.accountId
-        this.apiKey = options.apiKey
-        this.gql = new GraphQLClient(`${this.managerUrl}/gateway/api/graphql?accountId=${this.accountId}`, {
-            'x-api-key': this.apiKey,
-            'Content-Type': 'application/json',
-        })
+        this.managerUrl = new URL(options.managerUrl || defaultManagerUrl).origin
 
-        this.secrets = new Secrets(this.gql)
-        this.applications = new Applications(this.gql)
-        this.connectors = {
-            git: new GitConnectors(this.gql),
+        this.apiKey = options.apiKey
+        this.accountId = options.accountId
+
+        const headers: any = { }
+
+        if (this.apiKey) {
+            headers['x-api-key'] = this.apiKey
+        } else if (this.bearerToken) {
+            headers.authorization = `Bearer ${this.bearerToken}`
         }
-        this.environments = new Environments(this.gql, this)
-        this.cloudProviders = new CloudProviders(this.gql)
-        this.groups = new Groups(this.gql)
-        this.users = new Users(this.gql)
+        
+        this.client = new GraphQLClient(`${this.managerUrl}/gateway/api/graphql?accountId=${this.accountId}`, headers)
+
+        this.secrets = new Secrets(this.client)
+        this.applications = new Applications(this.client)
+        this.connectors = {
+            git: new GitConnectors(this.client),
+        }
+        this.environments = new Environments(this.client, this)
+        this.cloudProviders = new CloudProviders(this.client)
+        this.groups = new Groups(this.client)
+        this.users = new Users(this.client)
+    }
+
+    static async login(username: string, password: string, managerUrl?: string) {
+        const data = {
+            authorization: 'Basic ' + Buffer.from(username + ':' + password).toString('base64'),
+        }
+        
+        const response = await axios.post(`${new URL(managerUrl || defaultManagerUrl).origin}/gateway/api/users/login`, data)
+
+        return {
+            token: response.data.resource.token,
+            defaultAccountId: response.data.resource.defaultAccountId,
+            accounts: response.data.resource.accounts
+                .concat(response.data.resource.supportAccounts)
+                .map((a: { accountName: string; companyName: string; uuid: string }) => {
+                    return { name: a.accountName, company: a.companyName, id: a.uuid }
+                }),
+        }
     }
 }
