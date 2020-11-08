eb744439831627c32a9e89d6eaa7f23979eeada8
Luke Hertert <luke.hertert@gmail.com>
2020-11-08 15:29:53 -0500

    Create base command.  Update a couple commands to inherit from base

--- src/commands/application/create.ts
+++ src/commands/application/create.ts
@@ -1,40 +1,41 @@
-import { Command, flags } from '@oclif/command'
-import { Harness } from '../../providers/harness/harness-api-client'
+import { flags } from '@oclif/command'
+import { BaseCommand } from '../base-command'
 import { GitSyncOptions } from '../../providers/harness/applications'
 
-export default class ApplicationsCreate extends Command {
-  static description = 'Create a new application'
+export default class ApplicationsCreate extends BaseCommand {
+    static aliases = ['app:create', 'apps:create', 'applications:create', 'application:create']
 
-  static args = [
-      { name: 'name', description: 'The name of the application', required: true },
-      { name: 'description', description: 'A description of the application'},
-  ]
+    static description = 'Create a new Harness application'
 
-  static flags = {
-      syncEnabled: flags.boolean({ description: 'Whether or not git sync should be enabled', dependsOn: ['gitConnector'] }),
-      gitConnector: flags.string({ description: 'The name or id of the git connector to use for git sync' }),
-      branch: flags.string({ description: 'The branch name to use for git sync' }),
-      harnessAccountId: flags.string({ description: 'The Harness Account Id', required: true, env: 'HARNESS_ACCOUNT' }),
-      harnessApiKey: flags.string({ description: 'The Harness API Key', required: true, env: 'HARNESS_API_KEY' }),
-  }
+    static args = [
+        { name: 'name', description: 'The name of the application', required: true },
+        { name: 'description', description: 'A description of the application' },
+    ]
 
-  async run() {
-      const { args, flags } = this.parse(ApplicationsCreate)
+    static flags = {
+        ...BaseCommand.flags,
+        syncEnabled: flags.boolean({ description: 'Whether or not git sync should be enabled', dependsOn: ['gitConnector'] }),
+        gitConnector: flags.string({ description: 'The name or id of the git connector to use for git sync' }),
+        branch: flags.string({ description: 'The branch name to use for git sync' }),
+    }
 
-      const harness = new Harness({accountId: flags.harnessAccountId, apiKey: flags.harnessApiKey })
-      await harness.init()
+    async run() {
+        const { args, flags } = this.parse(ApplicationsCreate)
 
-      let gitSyncOptions: GitSyncOptions | undefined
-      if (flags.syncEnabled && flags.gitConnector) {
-          const connector = await harness.connectors.git.get(flags.gitConnector)
-          gitSyncOptions = {
-              syncEnabled: flags.syncEnabled,
-              gitConnectorId: connector.id,
-              branch: flags.branch,
-          }
-      }
+        const harness = await this.getHarnessClient()
 
-      const applications = await harness.applications.create(args.name, args.description, gitSyncOptions)
-      this.log(JSON.stringify(applications, undefined, 4))
-  }
+        let gitSyncOptions: GitSyncOptions | undefined
+
+        if (flags.syncEnabled && flags.gitConnector) {
+            const connector = await harness.connectors.git.get(flags.gitConnector)
+            gitSyncOptions = {
+                syncEnabled: flags.syncEnabled,
+                gitConnectorId: connector.id,
+                branch: flags.branch,
+            }
+        }
+
+        const applications = await harness.applications.create(args.name, args.description, gitSyncOptions)
+        this.log(applications)
+    }
 }
---
+++ src/commands/base-command.ts
@@ -0,0 +1,84 @@
+import Command, { flags } from '@oclif/command'
+import * as _ from 'lodash'
+import { Harness } from '../providers/harness/harness-api-client'
+import {Input, OutputArgs, OutputFlags } from '@oclif/parser'
+
+export abstract class BaseCommand extends Command {
+    static flags = {
+        debug: flags.boolean({ hidden: true, description: 'Print debug logs to stdout.' }),
+        jsonpath: flags.string({ hidden: true, description: 'Apply jsonpath expression to output prior to printing' }),
+        harnessAccountId: flags.string({ description: 'The Harness Account Id.  Can also be set via HARNESS_ACCOUNT environment variable.', env: 'HARNESS_ACCOUNT' }),
+        harnessApiKey: flags.string({ description: 'The Harness API Key. Can also be set via HARNESS_API_KEY environment variable.', env: 'HARNESS_API_KEY' }),
+    }
+
+    context!: {
+        args: OutputArgs<any>;
+        flags: OutputFlags<typeof BaseCommand.flags> ;
+        config: {
+            debug: boolean;
+            harness: {
+                accountId?: string,
+                apiKey?: string,
+            }
+        };
+        logger: {
+            log: (message: any) => void;
+            debug: (...args: any[]) => void;
+            error: { (input: string | Error, options: { code?: string | undefined; exit: false; }): void; (input: string | Error, options?: { code?: string | undefined; exit?: number | undefined; } | undefined): never; };
+        };
+    };
+
+    log(message: any) {
+        if (message && _.isObject(message)) {
+            console.log(JSON.stringify(message, undefined, 4))
+        } else {
+            console.log(message)
+        }
+    }
+    debug = (message: any) => {
+        if (this.context?.config.debug) {
+            this.log(message)
+        }
+    }
+
+    async init() {
+        // do some initialization
+        // const { args, flags } = this.parse(BaseCommand)
+        const { args, flags } = this.parse(this.constructor as Input<typeof BaseCommand.flags>)
+        const parsedArgs: OutputArgs<any> = args
+        const parsedFlags: OutputFlags<typeof BaseCommand.flags> = flags
+        this.context = {
+            args: parsedArgs,
+            flags: parsedFlags,
+            config: {
+                debug: parsedFlags.debug,
+                harness: {
+                    accountId: parsedFlags.harnessAccountId,
+                    apiKey: parsedFlags.harnessApiKey,
+                },
+            },
+            logger: {
+                log: this.log,
+                debug: this.debug,
+                error: this.error,
+            },
+        }
+
+        this.debug(flags)
+    }
+
+    async getHarnessClient() {
+        if (this.context.config.harness.accountId && this.context.config.harness.apiKey) {
+            const client = new Harness({ accountId: this.context.config.harness.accountId, apiKey: this.context.config.harness.apiKey})
+            try {
+                await client.init()
+                return client
+            } catch (error) {
+                this.error('Error initializing Harness API Client', { exit: false })
+                this.error(error, { exit: 1 })
+            }
+        } else {
+            this.error('The arguments harnessAccountId and harnessApiKey are required.')
+        }
+    }
+}
