0c8b4a755749eb6a59d51373e8d108d489f9170a
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2021-05-03 11:37:51 +0200

    Add secrets:get

---
+++ src/commands/secrets/get.ts
@@ -0,0 +1,25 @@
+import { BaseCommand as Command } from '../../base-command'
+import { flags } from '@oclif/command'
+import { SecretType } from '../../providers/harness/secrets'
+
+export default class SecretsGet extends Command {
+  static aliases = ['secret:get', 'secrets:get']
+  static description = 'Get Secret By Id or By Name'
+  static flags = {
+    ...Command.flags,
+    name: flags.string({ description: 'The name of the secret', char: 'n', exclusive: ['id'] }),
+    id: flags.string({ description: 'The id of the secret' }),
+    type: flags.enum({ options: [SecretType.Text], required: true, default: SecretType.Text }),
+  }
+
+  async run() {
+    const {args, flags} = this.parse(SecretsGet)
+    const harness = await this.getHarnessClient()
+    const nameOrId = flags.name || flags.id
+    if (!nameOrId) {
+        this.error('Either name or id is required.')
+    }
+    const secret = await harness.secrets.get(nameOrId, flags.type)
+    this.log(secret)
+  }
+}
