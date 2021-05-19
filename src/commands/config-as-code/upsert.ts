cd177fafa7719367518939f44a72f8ec89163a33
Luke Hertert <luke.hertert@gmail.com>
2021-05-18 21:46:30 -0400

    Deprecate username/password auth and make optional

--- src/commands/config-as-code/upsert.ts
+++ src/commands/config-as-code/upsert.ts
@@ -1,7 +1,6 @@
 import { BaseCommand as Command } from '../../base-command'
 import { HarnessStorageProvider } from '../../providers/storage/harness-api-storage'
 import { flags } from '@oclif/command'
-import { Harness } from '../../providers/harness/harness-api-client'
 
 export default class ConfigAsCodeUpsert extends Command {
     static aliases = [
@@ -19,20 +18,15 @@ export default class ConfigAsCodeUpsert extends Command {
         ...Command.flags,
         path: flags.string({ description: 'The file path', required: true }),
         content: flags.string({ description: 'The YAML content', required: true }),
-        harnessUsername: flags.string({ description: 'The Harness username. This is required for now until the underlying APIs suport API key auth.  Can also be set via HARNESS_USERNAME environment variable.', env: 'HARNESS_USERNAME', required: true }),
-        harnessPassword: flags.string({ description: 'The Harness password. This is required for now until the underlying APIs suport API key auth.  Can also be set via HARNESS_PASSWORD environment variable.', env: 'HARNESS_PASSWORD', required: true }),
+        harnessUsername: flags.string({ description: '[DEPRECATED] The Harness username. Can also be set via HARNESS_USERNAME environment variable.', env: 'HARNESS_USERNAME' }),
+        harnessPassword: flags.string({ description: '[DEPRECATED] The Harness password. Can also be set via HARNESS_PASSWORD environment variable.', env: 'HARNESS_PASSWORD' }),
     }
 
     async run() {
         const { flags } = this.parse(ConfigAsCodeUpsert)
 
-        const harness = new Harness({ accountId: this.context.config.harness.accountId || '', username: flags.harnessUsername, password: flags.harnessPassword })
-        try {
-            await harness.init()
-        } catch (error) {
-            this.error('Error initializing Harness API Client', { exit: false })
-            this.error(error, { exit: 1 })
-        }
+        const harness = await this.getHarnessClientDeprecated(flags.harnessUsername, flags.harnessPassword)
+
         const storageProvider = new HarnessStorageProvider({ accountId: harness.accountId }, harness)
         await storageProvider.init()
 
