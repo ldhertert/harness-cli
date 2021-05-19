9b6e83e0ce3e5fd710ab897134515fd9ae91a8d1
Luke Hertert <luke.hertert@gmail.com>
2021-05-19 02:59:35 -0400

    Allow saving to/loading from filesystem for config as code

--- src/commands/config-as-code/get.ts
+++ src/commands/config-as-code/get.ts
@@ -1,6 +1,7 @@
 import { BaseCommand as Command } from '../../base-command'
 import { HarnessStorageProvider } from '../../providers/storage/harness-api-storage'
 import { flags } from '@oclif/command'
+import { LocalStorageProvider } from '../../providers/storage/local-storage'
 
 export default class ConfigAsCodeGet extends Command {
     static aliases = ['config:get', 'config-as-code:get']
@@ -10,6 +11,7 @@ export default class ConfigAsCodeGet extends Command {
     static flags = {
         ...Command.flags,
         path: flags.string({ description: 'The file path(s) to fetch contents for. Glob patterns are supported.', required: true }),
+        out: flags.string({ description: 'A directory path on the local filesystem that will be used to write file contents to disk. '}),
         raw: flags.boolean({ description: 'Output raw YAML content instead of a JSON array.  This is only supported when there is a single file matching the provided path.' }),
         harnessUsername: flags.string({ description: '[DEPRECATED] The Harness username. Can also be set via HARNESS_USERNAME environment variable.', env: 'HARNESS_USERNAME' }),
         harnessPassword: flags.string({ description: '[DEPRECATED] The Harness password. Can also be set via HARNESS_PASSWORD environment variable.', env: 'HARNESS_PASSWORD' }),
@@ -32,6 +34,11 @@ export default class ConfigAsCodeGet extends Command {
             }
         } else {
             this.log(result)
+        }
+
+        if (flags.out) {
+            const localfs = new LocalStorageProvider({ directory: flags.out })
+            await localfs.storeFiles(result)
         }
     }
 }
--- src/commands/config-as-code/upsert.ts
+++ src/commands/config-as-code/upsert.ts
@@ -1,14 +1,15 @@
 import { BaseCommand as Command } from '../../base-command'
 import { HarnessStorageProvider } from '../../providers/storage/harness-api-storage'
 import { flags } from '@oclif/command'
+import { LocalStorageProvider } from '../../providers/storage/local-storage'
 
 export default class ConfigAsCodeUpsert extends Command {
     static aliases = [
-        'config:upsert', 
+        'config:upsert',
         'config-as-code:upsert',
-        'config:create', 
+        'config:create',
         'config-as-code:create',
-        'config:update', 
+        'config:update',
         'config-as-code:update',
     ]
 
@@ -16,8 +17,10 @@ export default class ConfigAsCodeUpsert extends Command {
 
     static flags = {
         ...Command.flags,
-        path: flags.string({ description: 'The file path', required: true }),
-        content: flags.string({ description: 'The YAML content', required: true }),
+        path: flags.string({ description: 'The file path', dependsOn: ['content'] }),
+        content: flags.string({ description: 'The YAML content', exclusive: ['files'] }),
+        source: flags.string({ description: 'The path to a directory on the local filesystem that contains files to be upserted.  Source files must contain a valid Harness config as code path structure relative to the source directory.', exclusive: ['content'] }),
+        sourcePattern: flags.string({ description: 'A glob pattern to limit files to include from source directory', default: '**/*.yaml' }),
         harnessUsername: flags.string({ description: '[DEPRECATED] The Harness username. Can also be set via HARNESS_USERNAME environment variable.', env: 'HARNESS_USERNAME' }),
         harnessPassword: flags.string({ description: '[DEPRECATED] The Harness password. Can also be set via HARNESS_PASSWORD environment variable.', env: 'HARNESS_PASSWORD' }),
     }
@@ -30,7 +33,16 @@ export default class ConfigAsCodeUpsert extends Command {
         const storageProvider = new HarnessStorageProvider({ accountId: harness.accountId }, harness)
         await storageProvider.init()
 
-        const result = await storageProvider.storeFile({ path: flags.path, content: flags.content })
-        this.log(result)
+        if (flags.path && flags.content) {
+            const result = await storageProvider.storeFile({ path: flags.path, content: flags.content })
+            this.log(result)
+        } else if (flags.source) {
+            const localfs = new LocalStorageProvider({ directory: flags.source })
+            const files = await localfs.getFiles(flags.sourcePattern)
+            const result = await storageProvider.storeFiles(files)
+            this.log(result)
+        } else {
+            this.error('You must provide either the source directory or the path/content values')
+        }
     }
 }
